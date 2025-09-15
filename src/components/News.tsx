68836e5e6afbb773aff0cf261960a1a5f8cdddef
bugsarebugs <joehacker9273@proton.me>
2025-09-15 12:40:09 -0400

    Fixed Everything Else

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -2,7 +2,7 @@
 export default function News(){
     return (
         <div className="overflow-x-hidden overflow-y-hidden font-bold bg-blue-500  ">
-            <ul className="horscroller text-black text-xs flex flex-row align-middle self-center-safe items-center-safe text-nowrap h-15 gap-10">
+            <ul className="horscroller text-black text-xs flex flex-row align-middle self-center-safe items-center-safe text-nowrap h-15 gap-10 text-white">
                 <li>We Speak Street and Gentle Codes.</li>
                 <li>brandMaina is not Just a Brand, Its the Next Gen HOPE</li>
                 <li>Love What You Wear. Fashion Is a Language, Speak it Fluently</li>
