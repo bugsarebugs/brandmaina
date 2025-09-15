212c06c407b49e6ee4c25b3ba51e52bdac9bed77
bugsarebugs <joehacker9273@proton.me>
2025-09-15 03:07:49 -0400

    Fixed Images Commit

--- src/components/ThirdSection.tsx
+++ src/components/ThirdSection.tsx
@@ -32,7 +32,7 @@ export default function ThirdSection() {
       {/* Image */}
       <div className="lg:w-1/2">
         <Image
-          src="/stupid9.png"
+          src="https://brandmaina.com/stupid9.png"
           alt="Msela tu huyu hapa"
           width={500}
           height={500}
